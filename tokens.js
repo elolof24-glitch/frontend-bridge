3b339fde1bea95d7eac7420a5a3f2a71691a0c0a
elolof24-glitch <elolof24@gmail.com>
2026-07-18 12:34:33 +0200

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,8 +1,19 @@
 const tokens = [
-  { id: 'usdc', name: 'USD Coin', symbol: 'USDC', logo: './logos/tokens/usdc.png' },
-  { id: 'weth', name: 'Wrapped Ether', symbol: 'WETH', logo: './logos/tokens/weth.png' },
-  { id: 'eth', name: 'Ether', symbol: 'ETH', logo: './logos/tokens/eth.png' },
-  { id: 'usdt', name: 'Tether', symbol: 'USDT', logo: './logos/tokens/usdt.png' },
-  { id: 'dai', name: 'Dai', symbol: 'DAI', logo: './logos/tokens/dai.png' },
-  { id: 'arb', name: 'Arbitrum', symbol: 'ARB', logo: './logos/tokens/arb.png' }
+  { id: 'usdt-eth', name: 'Tether', symbol: 'USDT', chainId: 'ethereum', logo: './logos/tokens/usdt.png' },
+  { id: 'usdt-arb', name: 'Tether', symbol: 'USDT', chainId: 'arbitrum', logo: './logos/tokens/usdt.png' },
+  { id: 'usdt-base', name: 'Tether', symbol: 'USDT', chainId: 'base', logo: './logos/tokens/usdt.png' },
+  { id: 'usdt-op', name: 'Tether', symbol: 'USDT', chainId: 'optimism', logo: './logos/tokens/usdt.png' },
+  { id: 'usdt-sol', name: 'Tether', symbol: 'USDT', chainId: 'solana', logo: './logos/tokens/usdt.png' },
+  { id: 'usdt-bnb', name: 'Tether', symbol: 'USDT', chainId: 'bnb', logo: './logos/tokens/usdt.png' },
+
+  { id: 'usdc-eth', name: 'USD Coin', symbol: 'USDC', chainId: 'ethereum', logo: './logos/tokens/usdc.png' },
+  { id: 'usdc-arb', name: 'USD Coin', symbol: 'USDC', chainId: 'arbitrum', logo: './logos/tokens/usdc.png' },
+  { id: 'usdc-base', name: 'USD Coin', symbol: 'USDC', chainId: 'base', logo: './logos/tokens/usdc.png' },
+  { id: 'usdc-sol', name: 'USD Coin', symbol: 'USDC', chainId: 'solana', logo: './logos/tokens/usdc.png' },
+
+  { id: 'sol-sol', name: 'Solana', symbol: 'SOL', chainId: 'solana', logo: './logos/tokens/sol.png' },
+  { id: 'bnb-bnb', name: 'BNB', symbol: 'BNB', chainId: 'bnb', logo: './logos/tokens/bnb.png' },
+  { id: 'eth-eth', name: 'Ether', symbol: 'ETH', chainId: 'ethereum', logo: './logos/tokens/eth.png' },
+  { id: 'eth-arb', name: 'Ether', symbol: 'ETH', chainId: 'arbitrum', logo: './logos/tokens/eth.png' },
+  { id: 'eth-base', name: 'Ether', symbol: 'ETH', chainId: 'base', logo: './logos/tokens/eth.png' }
 ];
