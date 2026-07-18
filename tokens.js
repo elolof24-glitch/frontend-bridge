7420f32de49d9ac00836b7dc029d4d8bd7450300
elolof24-glitch <elolof24@gmail.com>
2026-07-18 12:33:47 +0200

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,1 +1,8 @@
-
+const tokens = [
+  { id: 'usdc', name: 'USD Coin', symbol: 'USDC', logo: './logos/tokens/usdc.png' },
+  { id: 'weth', name: 'Wrapped Ether', symbol: 'WETH', logo: './logos/tokens/weth.png' },
+  { id: 'eth', name: 'Ether', symbol: 'ETH', logo: './logos/tokens/eth.png' },
+  { id: 'usdt', name: 'Tether', symbol: 'USDT', logo: './logos/tokens/usdt.png' },
+  { id: 'dai', name: 'Dai', symbol: 'DAI', logo: './logos/tokens/dai.png' },
+  { id: 'arb', name: 'Arbitrum', symbol: 'ARB', logo: './logos/tokens/arb.png' }
+];
