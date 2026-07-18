96b6c1971dfb932a43420e4a5846700576cb1491
elolof24-glitch <elolof24@gmail.com>
2026-07-18 12:34:47 +0200

    Update chains.js

--- chains.js
+++ chains.js
@@ -4,5 +4,6 @@ const chains = [
   { id: 'ethereum', name: 'Ethereum', logo: './logos/chains/ethereum.svg' },
   { id: 'base', name: 'Base', logo: './logos/chains/base.svg' },
   { id: 'optimism', name: 'Optimism', logo: './logos/chains/optimism.png' },
-  { id: 'solana', name: 'Solana', logo: './logos/chains/solana.png' }
+  { id: 'solana', name: 'Solana', logo: './logos/chains/solana.png' },
+  { id: 'bnb', name: 'BNB Chain', logo: './logos/chains/bnb.png' }
 ];
