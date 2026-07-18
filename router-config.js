fa0e458c1bbbe508c6823dd8f6610e89e4194f56
elolof24-glitch <elolof24@gmail.com>
2026-07-18 12:54:26 +0200

    Create router-config.js

---
+++ router-config.js
@@ -0,0 +1,20 @@
+window.ROUTER_CONFIG = {
+  relay: {
+    name: 'Relay Bridge',
+    short: 'Relay',
+    url: 'https://relay.link',
+    logo: './logos/routers/relay.png'
+  },
+  gaszip: {
+    name: 'Gas.zip',
+    short: 'Gas.zip',
+    url: 'https://www.gas.zip/',
+    logo: './logos/routers/gaszip.png'
+  },
+  across: {
+    name: 'Across Protocol',
+    short: 'Across',
+    url: 'https://across.to',
+    logo: './logos/routers/across.png'
+  }
+};
